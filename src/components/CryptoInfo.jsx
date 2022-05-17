89dca3254cfb0ac7111889e74115acc92aa24d8f
louismarques <louis.marques@edsx.ch>
2022-05-17 17:30:11 -0300

    Fix: info page now retrieves correct information on prices

--- src/components/CryptoInfo.jsx
+++ src/components/CryptoInfo.jsx
@@ -9,7 +9,9 @@ class CryptoInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      info: []
+      info: [],
+      usdprice: null,
+      btcprice: null
     };
   }
 
@@ -18,7 +20,19 @@ class CryptoInfo extends React.Component {
       .then(res => {
         console.log(res.data)
         const info = res.data;
-        this.setState({ info }, () => {
+        const usdprice = res.data.tickers.find(e => e.base===res.data.symbol.toUpperCase() && e.target==='USD')
+        var btcprice = []
+        if(res.data.symbol==='btc'){
+          btcprice = res.data.tickers.find(e => e.base==='ETH' && e.target==='BTC')
+          btcprice.last = 1/btcprice.last
+        }else{
+          btcprice = res.data.tickers.find(e => e.base===res.data.symbol.toUpperCase() && e.target==='BTC')
+          if (!btcprice) {
+            btcprice = res.data.tickers.find(e => e.base==='BTC' && e.target===res.data.symbol.toUpperCase())
+            btcprice.last = 1/btcprice.last
+          }
+        }
+        this.setState({ info,usdprice:usdprice.last,btcprice:btcprice.last}, () => {
           this.render();
         });
       })
@@ -38,8 +52,8 @@ class CryptoInfo extends React.Component {
                 <p><uppercase>{this.state.info['symbol']}</uppercase></p>
               </div>
               <div className="value">
-                <h2>{this.state.info.tickers && this.state.info.tickers[0].last}</h2>
-                <p>{this.state.info.tickers && this.state.info.tickers[0].last}</p>
+                <h2>{this.state.usdprice} USD</h2>
+                <p>{this.state.btcprice}{this.state.info['symbol']==='btc'?' ETH':' BTC'}</p>
               </div>
             </div>
 
@@ -75,7 +89,7 @@ class CryptoInfo extends React.Component {
               <div className="info"><h3>Price Change (24h)</h3><p>{this.state.info.market_data && this.state.info.market_data.price_change_24h}</p></div>
               <div className="info"><h3>Circulating Supply</h3><p>{this.state.info.market_data && this.state.info.market_data.circulating_supply}</p>
               </div>
-              <div className="info"><h3>24h Low / 24h High</h3><p>{this.state.info.market_data && this.state.info.market_data.high_24h['usd'] + " / " + this.state.info.market_data.low_24h['usd']}</p>
+              <div className="info"><h3>24h Low / 24h High</h3><p>{this.state.info.market_data && this.state.info.market_data.low_24h['usd'] + " / " + this.state.info.market_data.high_24h['usd']}</p>
               </div>
               <div className="info"><h3>Max Supply</h3><p>{this.state.info.market_data && this.state.info.market_data.total_supply}</p></div>
             </div>
